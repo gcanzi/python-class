f350c527622fccfdcb85e073eaadf45389e9a2af
gcanzi <gustavo.canzi@gmail.com>
2026-02-09 22:26:55 -0300

    Aula01

---
+++ Semestre02/Aula01/len.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls")
+
+list = [22,22,57.7,"logica"]
+qtd_elementos = len(list)
+
+'''
+.len = percorre toda a lista, contando a quantidade de elementos
+'''
+
+print(f"Quantidade = {qtd_elementos}")

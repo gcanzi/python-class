98226c03748dad0e14cd6c225848eb9188150928
gcanzi <gustavo.canzi@gmail.com>
2026-02-23 19:43:11 -0300

    sum

---
+++ Semestre02/Aula02/sum.py
@@ -0,0 +1,20 @@
+import os
+os.system("cls")
+
+lista = [56, 67, 89, 45, 34]
+soma = sum(lista)
+
+#.sum = soma todos os elementos da lista
+
+print(soma)
+print("------------------\n")
+
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+l3 = l1 + l2
+
+print(l1, l2, l3)
+print("------------------\n")
+
+l1 = l1 + l2
+print(l1, l2)
